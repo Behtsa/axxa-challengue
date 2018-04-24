3c924c3e80fc1598637827d4ae0e31f9abb2191f
superliza <eli_darko@hotmail.com>
2018-04-24 06:57:08 -0500

    integración de firebase

--- src/App.js
+++ src/App.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react';
 import { Route, Switch, Link } from 'react-router-dom';
 import InsuranceQuote from './components/InsuranceQuote';
+import './firebaseConfig';
+import firebase from 'firebase';
 
 class App extends Component {
+  constructor() {
+    super() 
+    this.database = firebase.database().ref().child('speed');
+
+    this.state = {
+      speed: 30
+    }
+  }
+
+  componentDidMount() {
+    this.database.on('value', snap => {
+      this.setState({
+        speed: snap.val()
+      });
+    });
+  }
+
   render() {
     return (
       <div>
         <InsuranceQuote />
       </div>
-    );
+    )
   }
 }
+
 
 export default App;
---
+++ src/firebaseConfig.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyA1Jjr1i4JXtew-Hoakg1Y0q_wYUJ4fImg",
+    authDomain: "fir-react-starter-32daf.firebaseapp.com",
+    databaseURL: "https://fir-react-starter-32daf.firebaseio.com",
+    projectId: "fir-react-starter-32daf",
+    storageBucket: "fir-react-starter-32daf.appspot.com",
+    messagingSenderId: "862609274252"
+};
+
+export default firebase.initializeApp(config);
