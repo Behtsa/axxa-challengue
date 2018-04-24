6beda7e6c92863599d8f3e22b398267669a397f4
superliza <eli_darko@hotmail.com>
2018-04-24 17:00:53 -0500

    hola

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,25 +1,28 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
+import JB from '../JB.png';
+import Cotiza from '../images/cotiza.png';
+import Serviceees from '../images/servicios.png';
+import SOS from '../images/sos.png';
+import Agente from '../images/agente.png';
+import Hamburger from './HamburgerMenu';
 
 export default class Services extends React.Component {
   render() {
     return (
       <Container>
+        <Hamburger />
         <Row>
-          <Col xs={{offset: 5 }}><img src="https://dummyimage.com/100x100/000/fff" alt="image"/></Col>
-          <Col className="hola" xs={{offset: 4 }}><h1> Servicios </h1></Col>
+          <Col xs={{offset: 3 }}><img className='img-responsive imgJB' src={JB} style={{ height: `200px`, width:`200px`}} alt="image"/></Col>
+          <Col className="holaa" xs={{offset: 3 }}><h1> Servicios </h1></Col>
         </Row>
         <Row>
-          <Col className="hola" xs={{offset: 2 }}><img src="https://dummyimage.com/100x100/000/fff" alt="image"/></Col>
-          <Col className="hola" xs={{offset: 2 }}><img src="https://dummyimage.com/100x100/000/fff" alt="image"/></Col>
+          <Col className="holaa" xs={{offset: 1 }}><img className='img-responsive' src={Cotiza} style={{ height: `100px`, width:`100px`}} alt="image"/></Col>
+          <Col className="holaa" xs={{offset: 2 }}><img className='img-responsive' src={Serviceees} style={{ height: `120px`, width:`120px`}} alt="image"/></Col>
         </Row>
         <Row>
-          <Col className="hola" xs={{offset: 2 }}><img src="https://dummyimage.com/100x100/000/fff" alt="image"/></Col>
-          <Col className="hola" xs={{offset: 2 }}><img src="https://dummyimage.com/100x100/000/fff" alt="image"/></Col>
-        </Row>
-        <Row>
-          <Col className="hola" xs={{offset: 5 }}><img src="https://dummyimage.com/100x100/000/fff" alt="image"/></Col>
-          
+          <Col className="holaa" xs={{offset: 1 }}><img className='img-responsive' src={SOS} style={{ height: `120px`, width:`120px`}} alt="image"/></Col>
+          <Col className="holaa" xs={{offset: 2 }}><img className='img-responsive' src={Agente} style={{ height: `120px`, width:`120px`}} alt="image"/></Col>
         </Row>
       </Container>
     );
